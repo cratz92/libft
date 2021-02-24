921d74e92a6905b4a91786d71781354bc5d02b8e
Carlos Leal <cbrito-l@student.42.fr>
2021-02-24 17:28:26 +0000

    quarto commit - atualizacao libft.h

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbrito-l <cbrito-l@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: cbrito-l <cbrito-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 23:53:01 by cbrito-l          #+#    #+#             */
-/*   Updated: 2021/02/24 04:25:47 by cbrito-l         ###   ########.fr       */
+/*   Updated: 2021/02/24 17:25:45 by cbrito-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,10 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t destSize);
 size_t	ft_strlcat(char *dest, const char *src, size_t destSize);
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+
 
 #endif
